811fb8db9fe5efc6339508e7a20396d0c5a938a8
baijian <baijian@gzads.com>
2020-12-06 10:59:06 +0800

    打印你好

---
+++ src/main/java/BaiTest.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @ClassName BaiTest
+ * @Author baijian
+ * @date 2020/12/06 10:57
+ */
+public class BaiTest {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
